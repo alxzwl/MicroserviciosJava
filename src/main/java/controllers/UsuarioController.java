45785cb041039547164d81e22eeaa9d903b699fd
Pc <alxzwl@gmail.com>
2022-05-14 15:40:46 -0500

    :art: utilizando el modelo usuario

--- src/main/java/controllers/UsuarioController.java
+++ src/main/java/controllers/UsuarioController.java
@@ -2,15 +2,23 @@ package controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import models.Usuario;
 
 @RestController
 public class UsuarioController {
 	
-	@RequestMapping(value="prueba")
-	public List<String> prueba() {
-		return List.of("Manzana","Kiwi", "Banana");
+	@RequestMapping(value = "usuario")
+	public Usuario getUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Lukas");
+		usuario.setApellido("Motola");
+		usuario.setEmail("motola@lukas.com");
+		usuario.setTelefono("5544332211");
+		usuario.setPassword("1234");
+		
+		return usuario;
 	}
 
 }
